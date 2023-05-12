fab9890e2a6c03f1b0ffc562187748b6a46b8823
Developer-Sahariar-Reza <dev.sahariar.reza@gmail.com>
2023-05-12 22:11:18 +0600

    Price calculation done

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 import "./Cart.css";
 
 const Cart = (props) => {
-    const cart = props.cart;
+    const {cart} = props;
     let totalPrice = 0;
-    
+    let totalShippingCharge = 0;
+    for(const product of cart){
+        totalPrice = totalPrice + product.price;
+        totalShippingCharge = totalShippingCharge + product.shipping;
+    }
+    const tax = totalPrice * 7 / 100;
+    const grandTotal = totalPrice + totalShippingCharge + tax;
     return (
         <div className='cart'>
             <h3 className='cart-headline'>Order Summary</h3>
             <div>
                 <p className='cart-details-common'>Selected Items: </p>
-                <p className='cart-details-common'>Total Price: $</p>
-                <p className='cart-details-common'>Shipping Charge: $</p>
-                <p className='cart-details-common'>Tax: $</p>
-                <h4 className='cart-grand-price'>Grand Total: $</h4>    
+                <p className='cart-details-common'>Total Price: ${totalPrice}</p>
+                <p className='cart-details-common'>Shipping Charge: ${totalShippingCharge}</p>
+                <p className='cart-details-common'>Tax: ${tax.toFixed(2)}</p>
+                <h4 className='cart-grand-price'>Grand Total: ${grandTotal.toFixed(2)}</h4>    
             </div>         
         </div>
     );
