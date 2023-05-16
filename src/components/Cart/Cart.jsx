2560ddebc56a367f14060481369ddc9b132ea530
Developer-Sahariar-Reza <dev.sahariar.reza@gmail.com>
2023-05-16 21:56:54 +0600

    display local storage cart ot the UI

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,28 +1,32 @@
-import React from 'react';
+import React from "react";
 import "./Cart.css";
 
 const Cart = (props) => {
-    const {cart} = props;
-    let totalPrice = 0;
-    let totalShippingCharge = 0;
-    for(const product of cart){
-        totalPrice = totalPrice + product.price;
-        totalShippingCharge = totalShippingCharge + product.shipping;
-    }
-    const tax = totalPrice * 7 / 100;
-    const grandTotal = totalPrice + totalShippingCharge + tax;
-    return (
-        <div className='cart'>
-            <h3 className='cart-headline'>Order Summary</h3>
-            <div>
-                <p className='cart-details-common'>Selected Items: </p>
-                <p className='cart-details-common'>Total Price: ${totalPrice}</p>
-                <p className='cart-details-common'>Shipping Charge: ${totalShippingCharge}</p>
-                <p className='cart-details-common'>Tax: ${tax.toFixed(2)}</p>
-                <h4 className='cart-grand-price'>Grand Total: ${grandTotal.toFixed(2)}</h4>    
-            </div>         
-        </div>
-    );
+  const { cart } = props;
+  let totalPrice = 0;
+  let totalShippingCharge = 0;
+  for (const product of cart) {
+    totalPrice = totalPrice + product.price;
+    totalShippingCharge = totalShippingCharge + product.shipping;
+  }
+  const tax = (totalPrice * 7) / 100;
+  const grandTotal = totalPrice + totalShippingCharge + tax;
+  return (
+    <div className="cart">
+      <h3 className="cart-headline">Order Summary</h3>
+      <div>
+        <p className="cart-details-common">Selected Items: {}</p>
+        <p className="cart-details-common">Total Price: ${totalPrice}</p>
+        <p className="cart-details-common">
+          Shipping Charge: ${totalShippingCharge}
+        </p>
+        <p className="cart-details-common">Tax: ${tax.toFixed(2)}</p>
+        <h4 className="cart-grand-price">
+          Grand Total: ${grandTotal.toFixed(2)}
+        </h4>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
